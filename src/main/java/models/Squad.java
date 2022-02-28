e0bc4a26785fb999fe80d166aaf4ecd36d7a2532
priscahlimo <priscahlimoh@gmail.com>
2022-02-28 07:41:52 +0300

    initiate squad test

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,14 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Squad {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public Squad(String name){
+        this.name = name;
+    }
 }
--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -9,6 +10,16 @@ public class SquadTest {
 
     @Before
     public void setUp() throws Exception {
+    }
+
+    public Squad setupSquad(){
+        return new Squad("flash");
+    }
+
+    @Test
+    public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
+        Squad squad = new Squad("flash");
+        assertEquals(true, squad instanceof Squad);
     }
 
     @After
